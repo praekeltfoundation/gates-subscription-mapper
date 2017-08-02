ef478bccd84cef555f148b2831bc5a2a49baf3ee
Rudi Giesler <rudi@praekelt.org>
2017-08-02 15:30:09 +0200

    Flake8 fixes for django generated files

--- mapper/admin.py
+++ mapper/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin
+from django.contrib import admin  # flake8: noqa
 
 # Register your models here.
--- mapper/tests.py
+++ mapper/tests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase  # flake8: noqa
 
 # Create your tests here.
