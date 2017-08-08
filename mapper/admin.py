34759252f622927a2466719fc060cfa8a1ae1f13
Rudi Giesler <rudi@praekelt.org>
2017-08-08 17:52:50 +0200

    Add django admin for migrate subscription model

--- mapper/admin.py
+++ mapper/admin.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.contrib import admin  # flake8: noqa
+from django import forms
+from django.db import models
+from django.contrib import admin
 
-# Register your models here.
+from .models import MigrateSubscription
+
+
+@admin.register(MigrateSubscription)
+class MigrateSubscriptionAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'created_at', 'completed_at', 'current', 'total', 'status', 'task_id')
+    date_hierarchy = 'created_at'
+    list_display = (
+        'task_id', 'status', 'table_name', 'column_name', 'current', 'total',
+        'created_at')
+    formfield_overrides = {
+        models.TextField: {'widget': forms.TextInput},
+    }
