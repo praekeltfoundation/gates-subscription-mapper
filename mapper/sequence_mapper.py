1fa599ec904077e6e7a6f63c3720a93865ecc94e
Rudi Giesler <rudi@praekelt.org>
2017-08-24 17:03:22 +0200

    Add sequence mapper class, for mapping logic

---
+++ mapper/sequence_mapper.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+class SequenceMapper(object):
+    """
+    Provides the logic for mapping the sequence number from one message set to
+    another.
+    """
+    def map(self, from_messageset, to_messageset, sequence):
+        # Default to no mapping
+        return sequence
+
+map_sequence = SequenceMapper().map
